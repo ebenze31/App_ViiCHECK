baef6488b8cbbe0020abd4b6590ccc3c3e0e4034
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-12-24 12:25:03 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <div class="title m-b-md">
-            {{ Auth::guard('web')->user()->name }}
+            {{ dd(session()->all()); }}
         </div>
     </div>
 @endsection
