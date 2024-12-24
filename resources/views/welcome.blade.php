a6991822a3e3b3d2840e20a89bd7d22075d315ee
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-12-24 12:25:42 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <div class="title m-b-md">
-            {{ dd(session()->all()); }}
+            {{ dd(session()->all()) }}
         </div>
     </div>
 @endsection
